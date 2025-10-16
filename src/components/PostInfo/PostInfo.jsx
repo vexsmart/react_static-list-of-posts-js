1f0ce14175bdb5bd3072ff6b16c396b9b9b12002
Vitor de Freitas Ribeiro <programador.vitor.ribeiro@hotmail.com>
2025-10-16 15:57:18 -0300

    under review

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,36 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post, users, comments }) => {
+  const postComments = comments.filter(comment => comment.postId === post.id);
+  const relatedUser = users.find(user => user.id === post.userId);
+  let commentsList = <CommentList comments={postComments} />;
+
+  if (postComments.length === 0) {
+    commentsList = (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    );
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={relatedUser} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {commentsList}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts, users, comments }) => {
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo key={post.id} comments={comments} users={users} post={post} />
+      ))}
+    </div>
+  );
+};
